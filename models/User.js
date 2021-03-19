52287c63200bd28963b199982db575a9aaa27474
UPye <75226236+UPye@users.noreply.github.com>
2021-03-19 03:19:23 -0400

    Created the login route for authentication using bcrypt

--- models/User.js
+++ models/User.js
@@ -1,8 +1,16 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
+// Creates hashing for user's password's to ensure they remain encrypted when stored
+const bcrypt = require('bcrypt');
+
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // Setup method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // create fields/columns for User model
 User.init(
@@ -34,6 +42,18 @@ User.init(
     }
   },
   {
+    hooks: {
+      // Setup beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+      },
+      // Setup beforeUpdate lifecycle "hook" functionality
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
