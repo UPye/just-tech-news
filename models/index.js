a28dbb8c2fb3fa422645c37eea27f4b687f4a4d7
UPye <75226236+UPye@users.noreply.github.com>
2021-03-20 00:35:11 -0400

    Defined model associations and created API routes for the post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,15 @@
 const User = require('./User');
-const Post = require('./Post')
+const Post = require('./Post');
+
+
+// Create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// Create reverse association
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
 
 module.exports = { User, Post };
