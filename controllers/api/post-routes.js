02b2d3d406e728425985de7376bf234a9f0a2d28
UPye <75226236+UPye@users.noreply.github.com>
2021-03-25 01:00:59 -0400

    Setup project and template engine, created homepage template, and populated template with sequelize data.

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,8 +3,8 @@ const sequelize = require('../../config/connection');
 const { Post, User, Comment, Vote } = require('../../models');
 
 // get all users
+
 router.get('/', (req, res) => {
-  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
@@ -13,7 +13,6 @@ router.get('/', (req, res) => {
       'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    order: [['created_at', 'DESC']],
     include: [
       {
         model: Comment,
@@ -29,7 +28,11 @@ router.get('/', (req, res) => {
       }
     ]
   })
-    .then(dbPostData => res.json(dbPostData))
+    .then(dbPostData => {
+      console.log(dbPostData[0]);
+      // pass a single post object into the homepage template
+      res.render('homepage', dbPostData[0]);
+    })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -142,5 +145,6 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
+
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
-router.use('/api', apiRoutes);
+const homeRoutes = require('./home-routes');
 
-router.use((req, res) => {
-  res.status(404).end();
-});
+router.use('/', homeRoutes);
+
+router.use('/api', apiRoutes);
 
 
 module.exports = router;
